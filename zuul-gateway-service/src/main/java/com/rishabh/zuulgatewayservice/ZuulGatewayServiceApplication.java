186c7cea143681849c35667ef809cc1febaf6d78
Rishabh Verma <rishabh.verma@yatra.com>
2020-11-30 18:24:52 +0530

    Added Zuul Gateway

---
+++ zuul-gateway-service/src/main/java/com/rishabh/zuulgatewayservice/ZuulGatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package com.rishabh.zuulgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ zuul-gateway-service/src/test/java/com/rishabh/zuulgatewayservice/ZuulGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.zuulgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
